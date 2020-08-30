71f5988db2c481f5a3420ee5550e056cbc68dae1
Алексей <televizor5259@gmail.com>
2020-08-30 20:30:12 +0300

    Добавляет тест разметку

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Technologies from "./Technologies";
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <Header/>
-        <Technologies/>
+    <div className='app-wrapper'>
+      <header className='page-header'>
+        <img className='page-logo' src="https://p.kindpng.com/picc/s/26-264795_fiat-decal-decorative-abarth-logo-png-transparent-png.png"
+             alt="логотип"/>
+      </header>
+      <nav className='main-nav'>
+        <ul className='main-nav__list'>
+          <li>
+            <a href="">Profile</a>
+          </li>
+          <li>
+            <a href="">Messages</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">Music</a>
+          </li>
+          <li>
+            <a href="">Settings</a>
+          </li>
+        </ul>
+      </nav>
+      <div className='main-content'>
+        <img src="https://i1.7fon.org/thumb/m527697.jpg"
+             alt=""/>
+
+        <section>
+          <img src=""
+               alt="Avatar"/>
+          <div>
+            <span>Description</span>
+          </div>
+        </section>
+
+        <section>
+          <h2>My Posts</h2>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </section>
       </div>
     </div>
   );
