627473e93741bbdb97de1d1ccfed7fe870e06b36
Алексей <televizor5259@gmail.com>
2020-08-30 11:30:05 +0300

    импортирует компоненты в App.js

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="Page-header">
+      <ul>
+        <li>
+          <a href="">user</a>
+        </li>
+        <li>
+          <a href="">news</a>
+        </li>
+        <li>
+          <a href="">about us</a>
+        </li>
+        <li>
+          <a href="">contacts</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
