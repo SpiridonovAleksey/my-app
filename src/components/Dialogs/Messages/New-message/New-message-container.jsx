c049556341105c3265859680f11db23f901bd13b
Алексей <televizor5259@gmail.com>
2021-01-30 21:35:30 +0300

    добавляет контейнер для компоненты отправки сообщений

---
+++ src/components/Dialogs/Messages/New-message/New-message-container.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './New-message.css';
+import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../../../redux/dialogs-reducer";
+
+const NewMessage = (props) => {
+  let newMessageText = React.createRef();
+
+  let sendMessage = () => {
+    props.dispatch(addMessageActionCreator());
+  };
+
+  let onMessageChange = () => {
+    let newText = newMessageText.current.value;
+    props.dispatch(updateNewMessageTextActionCreator(newText));
+  };
+
+  return (
+    <div className='new-message'>
+      <textarea className='new-message__text'
+                name=""
+                id=""
+                cols=""
+                rows=""
+                placeholder='Введите сообщение'
+                ref={newMessageText}
+                onChange={onMessageChange}
+                value={props.newMessageText}/>
+      <div className='new-message__control'>
+        <button className='new-message__button' onClick={sendMessage}>send</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewMessage;
