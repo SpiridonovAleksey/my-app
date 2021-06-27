bc6a12353777c7f633aaac267abd9dae38481773
Алексей <televizor5259@gmail.com>
2021-06-27 15:14:40 +0300

    .eslintcache to gitignore

--- src/components/Profile/User-general-wrap/User-general-wrap.jsx
+++ src/components/Profile/User-general-wrap/User-general-wrap.jsx
@@ -4,7 +4,6 @@ import GeneralAvatar from "./General-avatar/General-avatar.jsx";
 import UserInfo from "./User-info/User-info.jsx";
 
 const UserGeneralWrap = (props) => {
-
   return (
     <section className="user-general-wrap">
       <GeneralAvatar photos={props.profile.photos}/>
